41c1bd2be39b8a07412f036754d014de6a06f278
rails course from udemy <lomaslove132@gmail.com>
2019-05-04 22:48:41 +1000

    add validations for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,5 +8,9 @@ else
                       :path => ":style/:id_:filename"
 
 end
+validates :name, :description, :price, presence: true
+validates :price, numericality: { greater_than:0 }
+validates_attachment_presence :image
+
 end
 
